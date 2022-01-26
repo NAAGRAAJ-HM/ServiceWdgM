8e59599647536bddc30d92156c0d3cc5b8b9ef20
root <nagaraja.hm.0001@gmail.com>
2022-01-26 12:54:57 +0100

    Update: submodule WdgM

--- LATEST/WdgM.cpp
+++ LATEST/WdgM.cpp
@@ -5,3 +5,33 @@ class_WdgM WdgM;
 FUNC(void, WDGM_CODE) class_WdgM::InitFunction(void){
 }
 
+FUNC(void, WDGM_CODE) class_WdgM::DeInitFunction(void){
+}
+
+FUNC(void, WDGM_CODE) class_WdgM::GetVersionInfo(void){
+}
+
+FUNC(void, WDGM_CODE) class_WdgM::GetMode(void){
+}
+
+FUNC(void, WDGM_CODE) class_WdgM::SetMode(void){
+}
+
+FUNC(void, WDGM_CODE) class_WdgM::CheckpointReached(void){
+}
+
+FUNC(void, WDGM_CODE) class_WdgM::GetGlobalStatus(void){
+}
+
+FUNC(void, WDGM_CODE) class_WdgM::GetLocalStatus(void){
+}
+
+FUNC(void, WDGM_CODE) class_WdgM::PerformReset(void){
+}
+
+FUNC(void, WDGM_CODE) class_WdgM::GetFirstExpiredSEID(void){
+}
+
+FUNC(void, WDGM_CODE) class_WdgM::MainFunction(void){
+}
+
--- LATEST/WdgM.h
+++ LATEST/WdgM.h
@@ -5,7 +5,17 @@
 
 class class_WdgM{
    public:
-      FUNC(void, WDGM_CODE) InitFunction(void);
+      FUNC(void, WDGM_CODE) InitFunction        (void);
+      FUNC(void, WDGM_CODE) DeInitFunction      (void);
+      FUNC(void, WDGM_CODE) GetVersionInfo      (void);
+      FUNC(void, WDGM_CODE) GetMode             (void);
+      FUNC(void, WDGM_CODE) SetMode             (void);
+      FUNC(void, WDGM_CODE) CheckpointReached   (void);
+      FUNC(void, WDGM_CODE) GetGlobalStatus     (void);
+      FUNC(void, WDGM_CODE) GetLocalStatus      (void);
+      FUNC(void, WDGM_CODE) PerformReset        (void);
+      FUNC(void, WDGM_CODE) GetFirstExpiredSEID (void);
+      FUNC(void, WDGM_CODE) MainFunction        (void);
 };
 
 extern class_WdgM WdgM;
