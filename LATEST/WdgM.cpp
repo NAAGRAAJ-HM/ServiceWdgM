f4bc0d4a560302de5bb22026d897997d23a485c0
root <nagaraja.hm.0001@gmail.com>
2022-03-23 13:09:30 +0100

    Update: AUTOSAR basic software service module WdgM

--- LATEST/WdgM.cpp
+++ LATEST/WdgM.cpp
@@ -40,6 +40,16 @@ class module_WdgM:
       FUNC(void, WDGM_CODE) DeInitFunction (void);
       FUNC(void, WDGM_CODE) GetVersionInfo (void);
       FUNC(void, WDGM_CODE) MainFunction   (void);
+
+   private:
+      CONST(Std_TypeVersionInfo, WDGM_CONST) VersionInfo = {
+            0x0000
+         ,  0xFFFF
+         ,  0x01
+         ,  '0'
+         ,  '1'
+         ,  '0'
+      };
 };
 
 /******************************************************************************/
