b63442fca28b90ff13c5e678a545be35494acc56
root <nagaraja.hm.0001@gmail.com>
2022-03-31 12:05:26 +0200

    Update: AUTOSAR basic software service module WdgM

--- LATEST/WdgM.cpp
+++ LATEST/WdgM.cpp
@@ -37,10 +37,9 @@ class module_WdgM:
    public:
       module_WdgM(Std_TypeVersionInfo lVersionInfo) : abstract_module(lVersionInfo){
       }
-      FUNC(void, _CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, _CONFIG_DATA, _APPL_CONST) lptrCfgModule
+      FUNC(void, WDGM_CODE) InitFunction(
+         CONSTP2CONST(CfgModule_TypeAbstract, WDGM_CONFIG_DATA, WDGM_APPL_CONST) lptrCfgModule
       );
-      FUNC(void, WDGM_CODE) InitFunction   (void);
       FUNC(void, WDGM_CODE) DeInitFunction (void);
       FUNC(void, WDGM_CODE) MainFunction   (void);
 };
@@ -77,23 +76,39 @@ VAR(module_WdgM, WDGM_VAR) WdgM(
 /* FUNCTIONS                                                                  */
 /******************************************************************************/
 FUNC(void, WDGM_CODE) module_WdgM::InitFunction(
-   CONSTP2CONST(CfgWdgM_Type, CFGWDGM_CONFIG_DATA, CFGWDGM_APPL_CONST) lptrCfgWdgM
+   CONSTP2CONST(CfgModule_TypeAbstract, WDGM_CONFIG_DATA, WDGM_APPL_CONST) lptrCfgModule
 ){
-   if(NULL_PTR == lptrCfgWdgM){
+   if(E_OK == IsInitDone){
 #if(STD_ON == WdgM_DevErrorDetect)
       Det_ReportError(
       );
 #endif
    }
    else{
-// check lptrCfgWdgM for memory faults
+      if(NULL_PTR == lptrCfgModule){
+#if(STD_ON == WdgM_DevErrorDetect)
+         Det_ReportError(
+         );
+#endif
+      }
+      else{
+// check lptrCfgModule for memory faults
 // use PBcfg_WdgM as back-up configuration
+      }
+      IsInitDone = E_OK;
    }
-   WdgM.IsInitDone = E_OK;
 }
 
 FUNC(void, WDGM_CODE) module_WdgM::DeInitFunction(void){
-   WdgM.IsInitDone = E_NOT_OK;
+   if(E_OK != IsInitDone){
+#if(STD_ON == WdgM_DevErrorDetect)
+      Det_ReportError(
+      );
+#endif
+   }
+   else{
+      IsInitDone = E_NOT_OK;
+   }
 }
 
 FUNC(void, WDGM_CODE) module_WdgM::MainFunction(void){
