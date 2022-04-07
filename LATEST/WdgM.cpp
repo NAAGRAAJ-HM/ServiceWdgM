6ede30abe4551f7517e995f8f0626618566df024
root <nagaraja.hm.0001@gmail.com>
2022-04-07 09:51:52 +0200

    Update: AUTOSAR basic software service module WdgM

--- LATEST/WdgM.cpp
+++ LATEST/WdgM.cpp
@@ -96,8 +96,14 @@ FUNC(void, WDGM_CODE) module_WdgM::InitFunction(
 #endif
       }
       else{
+         if(STD_LOW){
 // check lptrCfgModule for memory faults
-// use PBcfg_WdgM as back-up configuration
+            lptrCfg = lptrCfgModule;
+         }
+         else{
+// use PBcfg_CanIf as back-up configuration
+            lptrCfg = PBcfg_CanIf;
+         }
       }
       IsInitDone = E_OK;
 #if(STD_ON == WdgM_InitCheck)
