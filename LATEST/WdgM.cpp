27d5cdd1b1c965f5d937fb3566a039cd0ff7a289
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-06-09 10:32:15 +0000

    Update: AUTOSAR basic software service module WdgM

--- LATEST/WdgM.cpp
+++ LATEST/WdgM.cpp
@@ -74,10 +74,10 @@ CONSTP2VAR(infSchMClient, WDGM_VAR, WDGM_CONST) gptrinfSchMClient_WdgM = &WdgM;
 VAR(module_WdgM, WDGM_VAR) WdgM(
    {
 #if(STD_ON == _ReSIM)
-   char strModuleName[6] = "WDGM";
+         "WdgM"
 #else
 #endif
-         WDGM_AR_RELEASE_VERSION_MAJOR
+      ,  WDGM_AR_RELEASE_VERSION_MAJOR
       ,  WDGM_AR_RELEASE_VERSION_MINOR
       ,  0x00
       ,  0xFF
