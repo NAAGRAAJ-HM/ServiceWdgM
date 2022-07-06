79537900ba12eba78f17835f74bc544fee0bc46f
NAGARAJA HM <nagaraja.hm.0001@gmail.com>
2022-07-06 17:32:05 +0000

    Update: AUTOSAR basic software service module for watchdog stack

--- LATEST/WdgM.cpp
+++ LATEST/WdgM.cpp
@@ -7,9 +7,7 @@
 /* #INCLUDES                                                                  */
 /******************************************************************************/
 #include "Module.hpp"
-#include "CfgWdgM.hpp"
-#include "WdgM_core.hpp"
-#include "infWdgM_Exp.hpp"
+#include "WdgM.hpp"
 #include "infWdgM_Imp.hpp"
 
 /******************************************************************************/
@@ -32,26 +30,6 @@
 /******************************************************************************/
 /* TYPEDEFS                                                                   */
 /******************************************************************************/
-class module_WdgM:
-      INTERFACES_EXPORTED_WDGM
-      public abstract_module
-   ,  public class_WdgM_Functionality
-{
-   private:
-/******************************************************************************/
-/* OBJECTS                                                                    */
-/******************************************************************************/
-
-   public:
-      FUNC(void, WDGM_CODE) InitFunction(
-         CONSTP2CONST(CfgModule_TypeAbstract, WDGM_CONFIG_DATA, WDGM_APPL_CONST) lptrCfgModule
-      );
-      FUNC(void, WDGM_CODE) DeInitFunction (void);
-      FUNC(void, WDGM_CODE) MainFunction   (void);
-      WDGM_CORE_FUNCTIONALITIES
-};
-
-extern VAR(module_WdgM, WDGM_VAR) WdgM;
 
 /******************************************************************************/
 /* CONSTS                                                                     */
