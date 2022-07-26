4ce485377fae9d4401536e5f3ff8583a1fc43b70
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2022-07-26 19:51:42 +0200

    Update: AUTOSAR basic software service module for watchdog stack

--- LATEST/WdgM.cpp
+++ LATEST/WdgM.cpp
@@ -61,7 +61,7 @@ FUNC(void, WDGM_CODE) module_WdgM::InitFunction(
             (NULL_PTR != lptrConstModule)
          && (NULL_PTR != lptrCfgModule)
       ){
-         lptrConst = lptrConstModule;
+         lptrConst = (const ConstWdgM_Type*)lptrConstModule;
          lptrCfg   = lptrCfgModule;
       }
       else{
