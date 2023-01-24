8179448bd654930cde8ad4129736fdd2d03b45eb
Nagaraja HM <nagaraja.hm.0001@gmail.com>
2023-01-24 21:49:35 +0100

    Update: AUTOSAR basic software service module for watchdog stack

---
+++ main_R2023/infServiceWdgM_ServiceNvM.hpp
@@ -0,0 +1,19 @@
+#pragma once
+/******************************************************************************/
+/* File   : infServiceWdgM_ServiceNvM.hpp                                     */
+/* Author : NAGARAJA HM (c) since 1982. All rights reserved.                  */
+/******************************************************************************/
+
+/******************************************************************************/
+/* #DEFINES                                                                   */
+/******************************************************************************/
+#define CONSTSERVICEWDGM_AR_RELEASE_VERSION_MAJOR                              4
+#define CONSTSERVICEWDGM_AR_RELEASE_VERSION_MINOR                              3
+
+#define Const_DefaultInit_ConstServiceWdgM                                                                               \
+   ,  {CONSTSERVICEWDGM_AR_RELEASE_VERSION_MAJOR, CONSTSERVICEWDGM_AR_RELEASE_VERSION_MINOR, 0x00,0xFF,0x01,'0','1','0'} \
+
+/******************************************************************************/
+/* EOF                                                                        */
+/******************************************************************************/
+
